c4be6505d194c53fdc8aa3b548b819da5f4a3ab7
Piyush Mohapatra <piyush.ofcl@gmail.com>
2023-08-09 12:17:45 +0530

    Delete Lab02 directory

--- Lab02/Labq1_dict.py
+++
@@ -1,8 +0,0 @@
-count = {}
-sent = input("Enter a Line : ")
-list_of_words = sent.split()
-for word in list_of_words:
-    count[word] = count.get(word, 0) + 1
-print('Frequency')
-for key in count.keys():
-    print(key, count[key])
