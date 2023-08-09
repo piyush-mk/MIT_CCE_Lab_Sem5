ce24e239eaeeca6032f5617978609a24cd5bfaa2
Piyush Mohapatra <piyush.ofcl@gmail.com>
2023-08-09 12:17:36 +0530

    Delete Lab01 directory

--- Lab01/Lab_add3.py
+++
@@ -1,6 +0,0 @@
-s=input("enter string: ")
-try:
-    int(s, 16)
-    print(s, "is a hexadecimal no.")
-except ValueError:
-    print(s, "is not a hexadecimal no.")
--- Lab01/Labq2_pattern.py
+++
@@ -1,6 +0,0 @@
-k = 1
-for i in range(5):
-    for j in range(i+1):
-        print(k, end='\t')
-        k+=1
-    print('\n')
